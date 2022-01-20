cdde60b41080141cfb5e4ef935d47530a6428faa
mpolosak <42770518+mpolosak@users.noreply.github.com>
2022-01-20 17:08:20 +0100

    Split processFile function into smaller functions

--- src/process.cpp
+++ src/process.cpp
@@ -1,25 +1,13 @@
 #include"process.hpp"
 #include<fstream>
 #include<iostream>
-#include<regex>
 
 using namespace std;
 
 int processFile(const ProgramOptions &options){
     const auto& [filepath, length, regex_str] = options;
-    fstream file(options.filepath);
-    if(!file){
-        cerr<<"Failed to open file '"<< filepath<<"'\n";
-        return 1;
-    }
-    std::regex regex;
-    try{
-        regex = std::regex(regex_str);
-    }
-    catch(const regex_error &error){
-        cerr<<"Incorrect regex format\n";
-        return 1;
-    }
+    auto file = openFileOrExit(filepath);
+    auto regex = createRegexOrExit(regex_str);
     string word;
     while(file){
         file>>word;
@@ -27,4 +15,23 @@ int processFile(const ProgramOptions &options){
             cout<<word<<"\n";
     }
     return 0;
+}
+
+fstream openFileOrExit(const string &path){
+    fstream file(path);
+    if(!file){
+        cerr<<"Failed to open file '"<< path<<"'\n";
+        exit(1);
+    }
+    return file;
+}
+
+regex createRegexOrExit(const string &regex_str){
+    try{
+        return regex(regex_str);
+    }
+    catch(const regex_error &error){
+        cerr<<"Incorrect regex format\n";
+        exit(1);
+    }
 }
--- src/process.hpp
+++ src/process.hpp
@@ -1,5 +1,8 @@
 #pragma once
 #include<string>
 #include"program_options.hpp"
+#include<regex>
 
 int processFile(const ProgramOptions &options);
+std::fstream openFileOrExit(const std::string &path);
+std::regex createRegexOrExit(const std::string &regex_str);
